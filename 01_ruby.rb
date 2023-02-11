d478e7923e37fe1cba87d1508fb9cae74c13512d
Tyler Jefferson <tylerrjefferson00@gmail.com>
2023-02-11 12:14:34 -0500

    first Ruby

---
+++ 01_ruby.rb
@@ -0,0 +1,5 @@
+puts 123
+puts 456
+puts 789
+
+# comments are done with a hash sign
---
+++ 02_ruby.rb
@@ -0,0 +1,35 @@
+# Objects
+
+## Ruby does not use 'primitives'
+## Most everything is an object
+
+### Objects are an instance of a Class, a Class is a general type while an Object is a specific instance of that type
+### Objects have behaviors that can be complex and interact with each other
+
+# ------------------------ #
+# Variables
+
+## Not objects, but keep track of objects and provide a way to reference them while programming
+## Once a var is assigned an object, it acts just like that object
+
+# ------------------------ #
+# Integer Methods
+
+# 100.Class
+# Integer
+
+# 100.next
+# 101
+
+# temp = -50
+# temp.abs (absolute value)
+# 50
+
+# ------------------------ #
+# Floats (floating point numbers / decimals)
+
+# price = 12.34
+# tax = 0.66
+# price + tax
+# 13.0 (adding a float to a float returns a float)
+
